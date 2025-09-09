1c063adac18e96efae7187588193f6edf25d393c
__Rtin__ <ntmute1@gmail.com>
2025-09-09 11:33:56 +0330

    created renderhtml files and added a demo template

---
+++ website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse , JsonResponse
+
+
+
+def index_view(request):
+    return render(request,'index.html')
+
+
+
+def about_view(request):
+    return render(request,'about.html')
+
+
+
+
+def contact_view(request):
+    return render(request,'index.html')
+
+
